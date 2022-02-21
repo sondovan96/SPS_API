b94341deb5c14c884778fb582888c2220cf76fa9
sondovan96 <60708028+sondovan96@users.noreply.github.com>
2022-02-21 16:24:01 +0700

    --Init project

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SPS_API.Controllers
+namespace SPS.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
