02f271e1db67306ed4f45367eab9d1901ef47741
phuvg <phuvg1996@gmail.com>
2022-03-05 15:18:32 +0700

    update category

---
+++ SPS.Service/Categorys/Commands/UpdateCategory/UpdateCategoryHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using SPS.Data.Repositories;
+using SPS.Data.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SPS.Service.Categorys.Commands.UpdateCategory
+{
+    public class UpdateCategoryHandler : IRequestHandler<UpdateCategoryRequest, bool>
+    {
+        private readonly IUnitOfWork _unitOfwork;
+        private readonly ICategoryRepo _categoryRepo;
+
+        public UpdateCategoryHandler(IUnitOfWork unitOfWork, ICategoryRepo categoryRepo)
+        {
+            _unitOfwork = unitOfWork;
+            _categoryRepo = categoryRepo;
+        }
+
+        public async Task<bool> Handle(UpdateCategoryRequest request, CancellationToken cancellationToken)
+        {
+            var category = await _categoryRepo.FindAsync(request.Id);
+            if(category != null)
+            {
+                if(request.Title != null)
+                {
+                    category.Title = request.Title;
+                }
+
+                if(request.MetaTitle != null)
+                {
+                    category.MetaTitle = request.MetaTitle;
+                }
+
+                if(request.ParentId != null)
+                {
+                    category.ParentId = request.ParentId;
+                }
+
+                if(request.IsDeleted == true)
+                {
+                    category.IsDeleted = request.IsDeleted;
+                }
+
+                _categoryRepo.Update(category);
+                await _unitOfwork.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SPS.Service/Categorys/Commands/UpdateCategory/UpdateCategoryRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SPS.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPS.Service.Categorys.Commands.UpdateCategory
+{
+    public class UpdateCategoryRequest : IRequest<bool>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string MetaTitle { get; set; }
+        public Guid? ParentId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
